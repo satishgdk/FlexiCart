56acd079ac266130da8a4fec060e557d9f428d53
ksatishvja <ksatishvja@gmail.com>
2020-08-27 12:44:17 +0530

    Implement Scenario3 ,  with Group Promotion like having C + D  in cart items

--- FlexiCart.Tests/ShoppingCartTests.cs
+++ FlexiCart.Tests/ShoppingCartTests.cs
@@ -132,6 +132,66 @@ namespace FlexiCart.Tests
             Assert.AreEqual(actual, expected);
         }
 
+        /// <summary>
+        /// Add a Test method get Active Promotions and apply to Cart with products
+        /// </summary>
+        [Test]
+        public void BuyProductsWithGroupPromotions()
+        {
+            //1 Arrange
+            // setup inputs 
+
+            Console.WriteLine("Perform Scenario3");
+            IRepository<Product> productRepository = new ProductRepository(new FakeDbContext());
+            var products = productRepository.GetAll();
+
+            IPromotionRepository<Promotion> promotionRepository = new PromotionRepository(new FakeDbContext());
+            var lstActivePromotions = promotionRepository.GetActiveRules().ToList();
+
+            /* 
+            Scenario
+
+            Setup unitPrices
+              Set Active Promotions
+              3 of A's for 130
+              2 of B's for  45
+              C & D for 30
+
+            5 *A 130 + 2*50
+            5 * B 45 +45 + 30
+            1 * C 20 
+            Total = 370
+
+             */
+
+
+            decimal expected = 280;
+            decimal actual = 0;// 
+
+            //2 Act
+
+            //  initialize cart
+
+            ShoppingCartModel cart = new ShoppingCartModel();
+            // addproducts
+
+            cart.AddProduct(products.First(p => p.SKU == "A"), 3);
+            cart.AddProduct(products.First(p => p.SKU == "B"), 5);
+            cart.AddProduct(products.First(p => p.SKU == "C"), 1);
+            cart.AddProduct(products.First(p => p.SKU == "D"), 1);
+            cart.AddPromotions(lstActivePromotions);
+            // setup promotions to cart 
+            // need to introduce a way to handle promotions to Cart and robust Product handling mechanism
+
+
+            actual = cart.GetTotal();
+
+
+            //3 Assert
+
+            Assert.AreEqual(actual, expected);
+        }
+
 
     }
 }
