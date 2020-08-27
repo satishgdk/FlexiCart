4c87f54be847a1a48c7ccfcd733dad1b56fa1b00
ksatishvja <ksatishvja@gmail.com>
2020-08-27 12:41:05 +0530

    Add Test method scenario2 with active promotions and products to the cart and check Total ,
    run previous test to verify nothing got effected.

--- FlexiCart.Tests/ShoppingCartTests.cs
+++ FlexiCart.Tests/ShoppingCartTests.cs
@@ -22,6 +22,9 @@ namespace FlexiCart.Tests
 
         }
 
+        /// <summary>
+        /// Add a Test method using AAA model in TDD and try to simply perform adding products to Cart and get total
+        /// </summary>
         [Test]
         public void BuySimpleProducts()
         {
@@ -68,6 +71,67 @@ namespace FlexiCart.Tests
 
             Assert.AreEqual(actual, expected);
         }
+
+        /// <summary>
+        /// Add a Test method get Active Promotions and apply to Cart with products
+        /// </summary>
+        [Test]
+        public void BuyProductsWithActivePromotions()
+        {
+            //1 Arrange
+            // setup inputs 
+
+            Console.WriteLine("Perform Scenario2");
+            IRepository<Product> productRepository = new ProductRepository(new FakeDbContext());
+            var products = productRepository.GetAll();
+
+            IPromotionRepository<Promotion> promotionRepository = new PromotionRepository(new FakeDbContext());
+            var lstActivePromotions = promotionRepository.GetActiveRules().ToList();
+
+            /* 
+            Scenario
+
+            Setup unitPrices
+              Set Active Promotions
+              3 of A's for 130
+              2 of B's for  45
+              C & D for 30
+
+            5 *A 130 + 2*50
+            5 * B 45 +45 + 30
+            1 * C 20 
+            Total = 370
+
+             */
+
+
+            decimal expected = 370;
+            decimal actual = 0;// 
+
+            //2 Act
+
+            //  initialize cart
+
+            ShoppingCartModel cart = new ShoppingCartModel();
+            // addproducts
+
+            cart.AddProduct(products.First(p => p.SKU == "A"), 5);
+            cart.AddProduct(products.First(p => p.SKU == "B"), 5);
+            cart.AddProduct(products.First(p => p.SKU == "C"), 1);
+            cart.AddPromotions(lstActivePromotions); 
+
+            // setup promotions to cart 
+            // need to introduce a way to handle promotions to Cart and robust Product handling mechanism
+
+
+            actual = cart.GetTotal();
+
+
+            //3 Assert
+
+            Assert.AreEqual(actual, expected);
+        }
+
 
     }
 }
