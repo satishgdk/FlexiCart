4dc32bf3314b77a267f15c94c93ad3b4eaa266b7
ksatishvja <ksatishvja@gmail.com>
2020-08-27 13:26:13 +0530

    Try Different case implementation with UnityContainer and moving object Dependency by Decoupling with Construction Injection

--- FlexiCart/Program.cs
+++ FlexiCart/Program.cs
@@ -5,6 +5,7 @@ using FlexiCart.Service.Interfaces;
 using System;
 using System.Linq;
 using Unity;
+using Unity.Injection;
 
 namespace FlexiCart
 {
@@ -19,12 +20,46 @@ namespace FlexiCart
 
 
             container.RegisterType<IProduct, Product>();
+            container.RegisterType<IPromotion, SimplePromotion>();
+
+            container.RegisterType<IPromotion, GroupedPromotion>("GroupedPromotion");
+
+            container.RegisterType<IPromotion, DiscountedPromotion>("DiscountedPromotion");
+
+            container.RegisterType<Criteria, Criteria>("DiscountedPromotion");
+
+            container.RegisterType<Criteria, DiscountCriteria>("DiscountCriteria");
+
+            container.RegisterType<IShoppingCartModel, ShoppingCartModel>("ShoppingCartModel");
+
+            container.RegisterType<IRepository<Product>, ProductRepository>(new InjectionConstructor(new FakeDbContext()));
+
+            container.RegisterType<IPromotionRepository<Promotion>, PromotionRepository>(new InjectionConstructor(new FakeDbContext()));
+
 
             #endregion
+
 
             ClientApp user1 = container.Resolve<ClientApp>();
 
             user1.BuySimpleProducts();
+
+            ClientApp user2 = container.Resolve<ClientApp>();
+
+            user2.BuyProductsWithActivePromotions();
+
+
+            ClientApp user3 = container.Resolve<ClientApp>();
+
+            user3.BuyProductsWithActiveGroupedPromotions();
+
+            ClientApp user4 = container.Resolve<ClientApp>();
+
+            user4.BuyProductsWithFestivalPromotions();
+
+            ClientApp user5 = container.Resolve<ClientApp>();
+
+            user5.BuyProducts();
 
             Console.ReadKey();
         }
@@ -36,6 +71,17 @@ namespace FlexiCart
     public class ClientApp
 
     {
+        private readonly IRepository<Product> _productRepository;
+        private readonly IPromotionRepository<Promotion> _promotionRepository;
+
+        public ClientApp(IRepository<Product> productRepository, IPromotionRepository<Promotion> promotionRepository)
+        {
+            _productRepository = productRepository;
+            _promotionRepository = promotionRepository;
+
+        }
+
+
         /// <summary>
         ///  User chosing simple products with different quantities and checking total of the Shopping cart 
         /// </summary>
@@ -43,7 +89,6 @@ namespace FlexiCart
         {
             Console.WriteLine("Buy Simple products \n");
 
-            IRepository<Product> _productRepository = new ProductRepository(new FakeDbContext());
             var products = _productRepository.GetAll();
 
             ShoppingCartModel cart = new ShoppingCartModel();
@@ -56,6 +101,95 @@ namespace FlexiCart
 
         }
 
+        /// <summary>
+        /// This method is to buy products considering Active Promotions
+        /// </summary>
+        public void BuyProductsWithActivePromotions()
+        {
+
+
+            Console.WriteLine("Buy  products with active promotions \n");
+            var products = _productRepository.GetAll();
+            var lstActivePromotions = _promotionRepository.GetActiveRules().ToList();
+
+            ShoppingCartModel cart = new ShoppingCartModel();
+
+            cart.AddProduct(products.First(p => p.SKU == "A"), 5);
+            cart.AddProduct(products.First(p => p.SKU == "B"), 5);
+            cart.AddProduct(products.First(p => p.SKU == "C"), 1);
+            cart.AddPromotions(lstActivePromotions);
+
+            cart.GetTotal();
+
+
+        }
+
+        /// <summary>
+        /// This method is to buy products considering Active Promotions with Group offer [C+D]
+        /// </summary>
+        public void BuyProductsWithActiveGroupedPromotions()
+        {
+
+            Console.WriteLine("Buy  products with active promotions with Group Promotion [C+D] \n");
+            var products = _productRepository.GetAll();
+            var lstActivePromotions = _promotionRepository.GetActiveRules().ToList();
+
+
+            ShoppingCartModel cart = new ShoppingCartModel();
+
+            cart.AddProduct(products.First(p => p.SKU == "A"), 3);
+            cart.AddProduct(products.First(p => p.SKU == "B"), 5);
+            cart.AddProduct(products.First(p => p.SKU == "C"), 1);
+            cart.AddProduct(products.First(p => p.SKU == "D"), 1);
+            cart.AddPromotions(lstActivePromotions);
+
+            cart.GetTotal();
+
+
+        }
+
+        /// <summary>
+        /// This method is to buy products considering system is having Festival Promotions. 
+        /// </summary>
+        public void BuyProductsWithFestivalPromotions()
+        {
+
+            Console.WriteLine("Buy  products with Festival promotions \n");
+            var products = _productRepository.GetAll();
+            var lstFestivalOffers = _promotionRepository.GetFestivalPromotions().ToList();
+
+
+            ShoppingCartModel cart = new ShoppingCartModel();
+
+            cart.AddProduct(products.First(p => p.SKU == "A"), 3);
+            cart.AddProduct(products.First(p => p.SKU == "B"), 5);
+            cart.AddProduct(products.First(p => p.SKU == "C"), 2);
+
+            cart.AddPromotions(lstFestivalOffers);
+
+            cart.GetTotal();
+
+        }
+
+
+        /// <summary>
+        /// This method is to buy products considering zero impact
+        /// </summary>
+        public void BuyProducts()
+        {
+
+            var products = _productRepository.GetAll();
+
+
+            ShoppingCartModel cart = new ShoppingCartModel();
+
+            cart.AddProduct(products.First(p => p.SKU == "A"), 3);
+            cart.AddProduct(products.First(p => p.SKU == "B"), 5);
+            cart.AddProduct(products.First(p => p.SKU == "C"), 2);
+
+            cart.GetTotal();
+
+        }
 
     }
 }
